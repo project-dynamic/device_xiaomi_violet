a3a497ad9bef20d832a6859364acc458ba46886e
romeo <rbesar210311@gmail.com>
2023-11-28 06:59:48 +0100

    violet : Bringup @ProjectMatrixx

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet vendor/xiaomi/violet --depth=1
+git clone https://github.com/joker-v2/kernel_xiaomi_violet kernel/xiaomi/violet --depth=1
+git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
+git clone --depth=1 https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229 -b master prebuilts/clang/host/linux-x86/clang-r498229
+git clone https://gitlab.com/NRanjan-17/android_vendor_MiuiCamera vendor/MiuiCamera --depth=1
+git clone https://github.com/tiann/KernelSU.git -b main kernel/xiaomi/violet/KernelSU
